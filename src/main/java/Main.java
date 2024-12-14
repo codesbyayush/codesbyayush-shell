69a4db4cf0593ea098ec177167c057871ceea4f7
Ayush Patel <codesbyayush@gmail.com>
2024-12-15 02:17:47 +0530

    shell-03

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,13 +2,15 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) throws Exception {
-        System.out.print("$ ");
-
         Scanner scanner = new Scanner(System.in);
+        boolean flag = true;
+        while(flag){
+        System.out.print("$ ");
         String input = scanner.nextLine();
 
         if(invalidCommand(input)){
             System.out.println(input + ": command not found");
+        }
         }
     }
 
