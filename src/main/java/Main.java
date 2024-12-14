bb8672c4dfe29d8ba09e4b1875ac8440c349e1af
Ayush Patel <codesbyayush@gmail.com>
2024-12-15 02:57:45 +0530

    shell-06

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,21 +1,24 @@
 import java.util.Scanner;
+import java.io.File;
 
 public class Main {
     public static void main(String[] args) throws Exception {
         Scanner scanner = new Scanner(System.in);
         boolean flag = true;
+
+        String[] paths = System.getenv("PATH").split(File.pathSeparator);
         while(flag){
             System.out.print("$ ");
             String input = scanner.nextLine();
 
             if(input.contains("type")) {
-                handleCommandType(input);
+                handleCommandType(input, paths);
             } else
             if (input.contains("exit")){
                 flag = false;
             } else if(input.contains("echo")) {
                 System.out.println(input.replace("echo ", ""));
-            }else
+            } else
             if(invalidCommand(input)){
                 System.out.println(input + ": command not found");
             }
@@ -26,7 +29,7 @@ public class Main {
         return true;
     }
 
-    static void handleCommandType(String input){
+    static void handleCommandType(String input, String[] paths){
         String command = input.replace("type ", "");
         switch (command) {
             case "exit":
@@ -39,6 +42,16 @@ public class Main {
                 System.out.println("type is a shell builtin");
                 break;
             default:
+                boolean found = false;
+                for(String path : paths){
+                    File file = new File(path + "/" + command);
+                    if(file.exists()){
+                        System.out.println(command + " is " + file.getAbsolutePath());
+                        found = true;
+                        break;
+                    }
+                }
+                if(!found)
                 System.out.println( command + ": not found");
         }
     }
