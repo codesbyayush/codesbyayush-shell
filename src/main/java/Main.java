60aee650b91dd72a569786537b8dfbed0f2300cb
Ayush Patel <codesbyayush@gmail.com>
2024-12-19 12:07:37 +0530

    shell-14

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -201,6 +201,7 @@ public class Main {
                             list.add("");
                         } else {
                             list.add(curr);
+
                         }
                     } else {
                         list.add(input.substring(stInd, nextInd));
